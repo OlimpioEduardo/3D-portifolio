f53e01fe7837f1c32e75e5e057d2d1e6f4de0d0f
Olimpio Eduardo <kamuenhoeduardo@75gmail.com>
2025-09-16 22:08:18 +0100

    TechIcons-Done

--- src/sections/TechStack.jsx
+++ src/sections/TechStack.jsx
@@ -1,7 +1,23 @@
-import TitleHeader from "../components/TitleHeader"
+import { useGSAP } from "@gsap/react"
+import TechIcon from "../components/Models/TechLogos/TechIcon.jsx"
+import TitleHeader from "../components/TitleHeader.jsx"
 import { techStackIcons } from "../constants"
+import gsap from "gsap"
 
 const TechStack = () => {
+    useGSAP(()=>{
+        gsap.fromTo('.tech-card', {y:50, opacity: 0}, {
+            y:0,
+            opacity: 1,
+            duration: 1,
+            ease: 'power2.inOut',
+            stragger: 0.2,
+            scrollTrigger:{
+            trigger:'#skils',
+            start:'top center'
+            }
+        })
+    }, [])
   return (
     <div id="skills" className="flex-center section-padding">
         <div className="w-full h-full md:px-10 px-5">
@@ -9,13 +25,39 @@ const TechStack = () => {
             title="My Preferred Tech Stack"
             sub="🫱🏽‍🫲🏾 The Skills I Bring to the Table"
             />
-
+                   {/* Display the icons */}
             <div className="tech-grid">
                 {techStackIcons.map((icon)=>(
                     <div key={icon.name} className="card-border tech-card overflow-hidden group xl:rounded-full rounded-lg">
-                        {icon.name}
+
+                    <div className="tech-card-animated-bg"/>
+                    <div className="tech-card-content">
+                        <div className="tech-icon-wrapper">
+                            <TechIcon model={icon}/>
+                        </div>
+
+                        <div className="padding-x w-full">
+                            <p>{icon.name}</p>
+                        </div>
+                    </div>
                     </div>
                 ))}
+
+              {/*  {techStackIcons.map((icon)=>(
+                    <div key={icon.name} className="card-border tech-card overflow-hidden group xl:rounded-full rounded-lg">
+
+                    <div className="tech-card-animated-bg"/>
+                    <div className="tech-card-content">
+                        <div className="tech-icon-wrapper">
+                           <img src={icon.imgPath}/>
+                        </div>
+
+                        <div className="padding-x w-full">
+                            <p>{icon.name}</p>
+                        </div>
+                    </div>
+                    </div>
+                ))} */}
             </div>
 
         </div>
